ddf5023f6034c29c777026738403d71641bf2f81
Yuan Li <liyuan6006@gmail.com>
2025-07-23 12:18:44 -0700

    update

---
+++ XRM360/Controllers/EducationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XRM360website.Controllers
+{
+    public class EducationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ XRM360/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XRM360website.Controllers
+{
+    public class HealthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
