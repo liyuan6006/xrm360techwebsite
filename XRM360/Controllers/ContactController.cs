cd75e24d62d6eb739b327712a800344f484210bc
Yuan Li <liyuan6006@gmail.com>
2025-07-23 13:56:44 -0700

    add contacus

---
+++ XRM360/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XRM360website.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
