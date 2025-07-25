32c393268e4373459411a5cae409f1851491bec4
Yuan Li <liyuan6006@gmail.com>
2025-07-25 13:47:20 -0700

    update tech page

---
+++ XRM360/Controllers/SoftwareSolutionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XRM360website.Controllers
+{
+    public class SoftwareSolutionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
