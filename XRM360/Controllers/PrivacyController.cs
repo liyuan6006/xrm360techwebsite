261e5d58e7e86d7bb700aa6664981f0b7edbe15a
Yuan Li <liyuan6006@gmail.com>
2025-07-26 15:12:58 -0700

    add privacy

---
+++ XRM360/Controllers/PrivacyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XRM360website.Controllers
+{
+    public class PrivacyController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return LangView("Index");
+        }
+    }
+}
