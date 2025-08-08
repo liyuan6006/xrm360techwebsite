384fdd3b1bbc70d4c62a0cc1f5163466fb2133dd
Yuan Li <liyuan6006@gmail.com>
2025-08-08 10:07:27 -0700

    redirect www to non www

--- XRM360/Program.cs
+++ XRM360/Program.cs
@@ -11,6 +11,23 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    // Middleware to redirect www.xrm-360.com ? xrm-360.com
+    app.Use(async (context, next) =>
+    {
+        var host = context.Request.Host.Host;
+
+        if (host.StartsWith("www.", StringComparison.OrdinalIgnoreCase))
+        {
+            var newHost = host.Substring(4); // remove "www."
+            var newUrl = $"https://{newHost}{context.Request.PathBase}{context.Request.Path}{context.Request.QueryString}";
+
+            context.Response.Redirect(newUrl, permanent: true);
+            return;
+        }
+
+        await next();
+    });
 }
 
 app.UseHttpsRedirection();
